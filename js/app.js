6af81a645dd70bd9fc4945d443273f294c2013cd
Rootpaceusa <64696314+ahmadchenwangxuesi@users.noreply.github.com>
2020-05-08 00:33:11 +0700

    work

---
+++ js/app.js
@@ -0,0 +1,13 @@
+window.Vue = require('vue');
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+Vue.component('video-component', require('./components/VideoComponent.vue').default);
+
+const app = new Vue({
+    el: '#app'
+});
